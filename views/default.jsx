042a624cf8049f729bb2ce6152a10d8e0e87b815
ZohaibFattani <fattanizohaib@gmail.com>
2022-09-20 20:33:16 -0700

    adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,19 @@
+//imports react
+let React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+  }
+  
+
+//exports Def function
+module.exports = Def
