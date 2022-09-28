aea858f1e2021b44c16c0e08cac3c53849667acd
ZohaibFattani <fattanizohaib@gmail.com>
2022-09-27 18:05:26 -0700

    finished edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -2,11 +2,13 @@ const React = require('react')
 const Def = require('../default.jsx')
 
 function edit_form (data) {
+  console.log("------")
+  console.log(data)
     return (
         <Def>
           <main>
             <h1>Edit Place</h1>
-<form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
+<form method="POST" action={`/places/${data.id}?_method=PUT`}>
     <div className="row">
   <div className="form-group">
     <label htmlFor="name">Place Name</label>
