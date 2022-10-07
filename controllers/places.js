bcabcc1249fac394a38765a4e3671cb4819d3478
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-06 19:29:59 -0700

    added comments to show.jsx

--- controllers/places.js
+++ controllers/places.js
@@ -36,16 +36,19 @@ router.get("/new", (req, res) => {
   res.render("places/new");
 });
 
-router.get("/:id", (req, res) => {
+router.get('/:id', (req, res) => {
   db.Place.findById(req.params.id)
-    .then((place) => {
-      res.render("places/show", { place });
-    })
-    .catch((err) => {
-      console.log("err", err);
-      res.render("error404");
-    });
-});
+  .populate('comments')
+  .then(place => {
+      console.log(place.comments)
+      res.render('places/show', { place })
+  })
+  .catch(err => {
+      console.log('err', err)
+      res.render('error404')
+  })
+})
+
 
 router.put("/:id", (req, res) => {
   res.send("PUT /places/:id stub");
